eabd02efe1270f3c25b4161c930e555ee882ec07
Mattias Hising <hising@gmail.com>
2017-02-07 15:50:46 +0100

    wip

--- docs/js/components/map.js
+++ docs/js/components/map.js
@@ -5,10 +5,10 @@ export const Map = React.createClass({
   componentDidMount: function() {
     let map = this.map = L.map(ReactDOM.findDOMNode(this), {
       minZoom: 2,
-      maxZoom: 18,
+      maxZoom: 19,
       layers: [
-        L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-          maxZoom: 20,
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
           attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         })
       ],
