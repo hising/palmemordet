bf9ccc128799c1f211f9d11b08aaa5787a3cc3f0
Mattias Hising <hising@gmail.com>
2017-02-07 11:05:32 +0100

    Refactored GA-code a bit (make it understandable)

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components';
 
-/*eslint-disable */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+(function(window, document, tagName, scriptSource, trackerName){
+
+    window['GoogleAnalyticsObject'] = trackerName;
+
+    window[trackerName] = window[trackerName] || function(){
+        (window[trackerName].q = window[trackerName].q || []).push(arguments);
+    };
+
+    window[trackerName].l = 1 * new Date();
+    let scriptElement = document.createElement(tagName);
+    let scriptCollection = document.getElementsByTagName(tagName)[0];
+    scriptElement.async = 1;
+    scriptElement.src = scriptSource;
+    scriptCollection.parentNode.insertBefore(scriptElement, scriptCollection);
+
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-/*eslint-enable*/
 
 ga('create', 'UA-186744-112', 'auto');
 ga('send', 'pageview');
