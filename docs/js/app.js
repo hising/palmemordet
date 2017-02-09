e47ab35f398c8b69d37508c93ee9a5d44b0e1950
Mattias Hising <hising@gmail.com>
2017-02-09 19:52:44 +0100

    hashHistory instead of browserHistory

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,18 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App, Video, Timeline} from './components';
 import {analytics} from './analytics';
-import {Router, Route, browserHistory} from 'react-router';
-import {isDev} from './utils';
-
-let rootRoute = isDev ? '/' : '/palmemordet/';
-let timelineRoute = rootRoute + 'timeline';
-let videoRoute = rootRoute + 'video';
+import {Router, Route, hashHistory} from 'react-router';
 
 let routing = (
-  <Router history={browserHistory}>
-    <Route path={rootRoute} component={App}>
-      <Route path={timelineRoute} component={Timeline}/>
-      <Route path={videoRoute} component={Video}/>
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <Route path="timeline" component={Timeline}/>
+      <Route path="video" component={Video}/>
     </Route>
   </Router>
 );
