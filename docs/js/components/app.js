3131a60169f4c27ea3b07a4aafd0e508b1799eb6
Mattias Hising <hising@gmail.com>
2017-02-11 11:36:22 +0100

    Added manifest and pseudo-code for messaging via Firebase

--- docs/js/components/app.js
+++ docs/js/components/app.js
@@ -2,6 +2,32 @@ import React from 'react';
 import {NavLink} from './navlink';
 import {Home} from './home';
 
+/*
+import * as firebase from 'firebase';
+
+let config = {
+  apiKey: 'AIzaSyAUjD0R-x5pEvCm7JLCIRGsJYGfcA28rRU',
+  authDomain: 'palmemordet-430a4.firebaseapp.com',
+  databaseURL: 'https://palmemordet-430a4.firebaseio.com',
+  storageBucket: 'palmemordet-430a4.appspot.com',
+  messagingSenderId: '237810585662'
+};
+
+firebase.initializeApp(config);
+
+const messaging = firebase.messaging();
+
+messaging.requestPermission()
+  .then(function() {
+    console.log('Notification permission granted.');
+    // TODO(developer): Retrieve an Instance ID token for use with FCM.
+    // ...
+  })
+  .catch(function(err) {
+    console.log('Unable to get permission to notify.', err);
+  });
+  */
+
 export const App = React.createClass({
   render() {
     return (
